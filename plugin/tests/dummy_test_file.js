578c2c956cb81193eb43e570a57b766303af191a
toran billups <toranb@gmail.com>
2014-05-31 17:10:52 -0500

    now finds any it nested inside a describe block

--- plugin/tests/dummy_test_file.js
+++ plugin/tests/dummy_test_file.js
@@ -1,4 +1,5 @@
-
-it("test example", function() {
-  equal(1, 1, "The values are equal");
+describe("react task integration tests", function() {
+    it("test example", function() {
+      equal(1, 1, "The values are equal");
+    });
 });
--- plugin/vim_mocha_special_blend.py
+++ plugin/vim_mocha_special_blend.py
@@ -2,16 +2,16 @@ import re
 
 
 def sub_current_test_with_singleTest(module_line_num, current_buffer):
-    return re.sub(r"^it", r"it.only", current_buffer[module_line_num])
+    return re.sub(r"^it", r"it.only", current_buffer[module_line_num].lstrip())
 
 
 def sub_singleTest_with_test(module_line_num, current_buffer):
-    return re.sub(r"^it.only", r"it", current_buffer[module_line_num])
+    return re.sub(r"^it.only", r"it", current_buffer[module_line_num].lstrip())
 
 
 def get_line_num_of_current_test(current_line_index, current_buffer):
     test_regex = re.compile(r"^it")
     for line_num in xrange(current_line_index - 1, -1, -1):
-        if test_regex.search(current_buffer[line_num]) is not None:
+        if test_regex.search(current_buffer[line_num].lstrip()) is not None:
             return line_num
     return False
